a391ca16cbc716796b25fa92bb6286739a85638a
Charlie Zhang <chuanchuanzhang@trip.com>
2023-03-07 19:37:20 +0800

    impromve action

--- Assets/Scripts/Characters/Boy_01_Action.cs
+++ Assets/Scripts/Characters/Boy_01_Action.cs
@@ -3,63 +3,201 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-abstract class Boy_01_ActionStatus
-{
-    protected Boy_01_Action mAction;
-
-    public Boy_01_ActionStatus(Boy_01_Action action)
-    {
-        this.mAction = action;
-    }
-
-    abstract void FixedUpdate();
-}
-
-class Boy_01_ActionStatus_Idle : Boy_01_ActionStatus
-{
-    void FixedUpdate()
-    {
-        float forwarding = Input.GetAxis("Vertical");
-        // float turning = Input.GetAxis("Horizontal");
-        if (forwarding != 0.0f)
-        {
-            // change to walk status
-            mAction.SetStatus()
-        }
-    }
-}
-
-
-
-
 public class Boy_01_Action : MonoBehaviour
 {
+    abstract public class Boy_01_ActionStatus
+    {
+        protected Boy_01_Action mAction;
+        protected string mStatusName;
+        protected int mStatusCode;
+
+        public Boy_01_ActionStatus(Boy_01_Action action, string name)
+        {
+            mAction = action;
+            mStatusName = name;
+            mStatusCode = Animator.StringToHash(name);
+        }
+
+        public string GetStatusName() { return mStatusName; }
+        public int GetStatusCode() { return mStatusCode; }
+        virtual public void Start()
+        {
+            // Debug.Log("entering status: " + mStatusName);
+            mAction.mAnimator.CrossFade(mStatusCode, 0.1f, 0, 0);
+        }
+        virtual public void Update(float deltaTime) { }
+    }
+
+    class Boy_01_ActionStatus_Idle : Boy_01_ActionStatus
+    {
+        public Boy_01_ActionStatus_Idle(Boy_01_Action action)
+            : base(action, "Base Layer.idle") { }
+
+        override public void Update(float deltaTime)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                // start to jump
+                mAction.SetStatus("Base Layer.jump");
+                return;
+            }
+
+            float forwarding = Input.GetAxis("Vertical");
+            if (forwarding > 0.0f)
+            {
+                // change to move status
+                mAction.SetStatus("Base Layer.move");
+                return;
+            }
+        }
+    }
+
+    class Boy_01_ActionStatus_Move : Boy_01_ActionStatus
+    {
+        protected int mSpeedKey;
+        protected float mSpeed;
+
+        public Boy_01_ActionStatus_Move(Boy_01_Action action)
+            : base(action, "Base Layer.move") 
+        {
+            mSpeedKey = Animator.StringToHash("Speed");
+        }
+
+        override public void Update(float deltaTime)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                // start to jump
+                mAction.SetStatus("Base Layer.jump");
+                return;
+            }
+
+            float forwarding = Input.GetAxis("Vertical");
+            if (forwarding <= 0.0f)
+            {
+                // back to idle
+                mAction.SetStatus("Base Layer.idle");
+                return;
+            }
+
+            mAction.SetMovingSpeedUp(forwarding);
+
+            float curSpeed = mAction.GetMovingSpeed();
+            if (curSpeed != mSpeed)
+            {
+                mSpeed = curSpeed;
+                mAction.mAnimator.SetFloat(mSpeedKey, mSpeed);
+            }
+
+            float turning = Input.GetAxis("Horizontal");
+            mAction.SetTurningSpeedUp(turning);
+
+            forwarding *= mAction.GetMovingSpeed() * deltaTime;
+            turning *= mAction.GetTurningSpeed() * deltaTime;
+
+            mAction.transform.Rotate(Vector3.up, turning);
+            mAction.mController.Move(mAction.transform.forward * forwarding);
+        }
+    }
+
+    class Boy_01_ActionStatus_Jump : Boy_01_ActionStatus
+    {
+        protected int mSpeedKey;
+        protected float mMoveSpeed;
+        protected float mJumpSpeed;
+        protected bool mUpdated;
+
+        public Boy_01_ActionStatus_Jump(Boy_01_Action action)
+            : base(action, "Base Layer.jump")
+        {
+            mSpeedKey = Animator.StringToHash("JumpPose");
+        }
+
+        public override void Start()
+        {
+            base.Start();
+            mUpdated = false;
+            mMoveSpeed = mAction.GetMovingSpeed();
+            mJumpSpeed = mAction.GetJumpingSpeed();
+            mAction.mAnimator.SetFloat(mSpeedKey, 8.0f);
+        }
+
+        override public void Update(float deltaTime)
+        {
+            // Debug.Log(deltaTime.ToString());
+
+            if (mUpdated && mAction.mController.isGrounded)
+            {
+                // ÆðÌøºóÂäµØ
+                if (mMoveSpeed > 0.0f)
+                {
+                    mAction.SetStatus("Base Layer.move");
+                }
+                else
+                {
+                    mAction.SetStatus("Base Layer.idle");
+                }
+            }
+            else
+            {
+                if (!mUpdated) { mUpdated = true; }
+
+                var direction = mAction.transform.forward * mMoveSpeed * deltaTime;
+                if (mJumpSpeed > 0.0f)
+                {
+                    var diffJumpSpeed = deltaTime * 9.8f;
+                    var curJumpSpeed = mJumpSpeed - diffJumpSpeed;
+                    if (curJumpSpeed > 0.0f)
+                    {
+                        direction += Vector3.up * (mJumpSpeed - (diffJumpSpeed * 0.5f)) * deltaTime;
+                    }
+                    mJumpSpeed = curJumpSpeed;
+                }
+
+                mAction.mController.Move(direction);
+            }
+        }
+    }
+
+    public float moveSpeed = 1.0f;
+    public float turnSpeed = 100.0f;
+    public float jumpSpeed = 100.0f;
+
+    protected float mMoveSpeedUp = 0.0f;
+    protected float mTurnSpeedUp = 0.0f;
+
     protected Animator mAnimator;
     protected CharacterController mController;
     protected Boy_01_ActionStatus mStatus;
-    protected Dictionary<int, Boy_01_ActionStatus> mStatusStore;
+    protected Dictionary<string, Boy_01_ActionStatus> mStatusStore;
 
-    public float forwardSpeed = 1.0f;
-    public float turnSpeed = 100.0f;
-
-    private static readonly int mStatusCodeIdle = Animator.StringToHash("Base Layer.idle");
-    private static readonly int mStatusCodeMove = Animator.StringToHash("Base Layer.move");
-    private static readonly int mStatusCodeJump = Animator.StringToHash("Base Layer.jump");
-    private static readonly int mStatusCodeDamage = Animator.StringToHash("Base Layer.damage");
-    private static readonly int mStatusCodeDown = Animator.StringToHash("Base Layer.down");
-    private static readonly int mStatusCodeFaint = Animator.StringToHash("Base Layer.faint");
-    private static readonly int mStatusCodeStandupFaint = Animator.StringToHash("Base Layer.standup_faint");
-
-    private static readonly int JumpTag = Animator.StringToHash("Jump");
-    private static readonly int DamageTag = Animator.StringToHash("Damage");
-    private static readonly int FaintTag = Animator.StringToHash("Faint");
-
-    private static readonly int SpeedParameter = Animator.StringToHash("Speed");
-    private static readonly int JumpPoseParameter = Animator.StringToHash("JumpPose");
-
-    void SetStatus(int statusCode)
+    void SetMovingSpeedUp(float speedUp)
+    { 
+        mMoveSpeedUp = speedUp > 0.0f ? speedUp : -speedUp;
+    }
+    void SetTurningSpeedUp(float speedUp)
     {
-        mStatusStore.TryGetValue(statusCode, out mStatus);
+        mTurnSpeedUp = speedUp > 0.0f ? speedUp : -speedUp;
+    }
+    float GetMovingSpeed() { return moveSpeed * mMoveSpeedUp; }
+    float GetTurningSpeed() { return turnSpeed * mTurnSpeedUp; }
+    float GetJumpingSpeed() { return jumpSpeed; }
+
+    void SetStatus(string statusName)
+    {
+        Boy_01_ActionStatus status;
+        if (!mStatusStore.TryGetValue(statusName, out status))
+        {
+            return;
+        }
+
+        if (mStatus.GetStatusCode() == status.GetStatusCode())
+        {
+            return;
+        }
+
+        mStatus = status;
+        mStatus.Start();
     }
 
     // Start is called before the first frame update
@@ -67,30 +205,52 @@ public class Boy_01_Action : MonoBehaviour
     {
         mAnimator = GetComponent<Animator>();
         mController = GetComponent<CharacterController>();
-        mStatusStore = new Dictionary<int, Boy_01_ActionStatus>
+
+        var statusIdle = new Boy_01_ActionStatus_Idle(this);
+        var statusMove = new Boy_01_ActionStatus_Move(this);
+        var statusJump = new Boy_01_ActionStatus_Jump(this);
+
+        mStatusStore = new Dictionary<string, Boy_01_ActionStatus>
         {
-            (Animator.StringToHash("Base Layer.idle"), new Boy_01_ActionStatus()),
+            { statusIdle.GetStatusName(), statusIdle },
+            { statusMove.GetStatusName(), statusMove },
+            { statusJump.GetStatusName(), statusJump },
+        };
+        mStatus = statusIdle;
+        mStatus.Start();
+    }
+
+    void UpdateSpeedUp(float deltaTime)
+    {
+        if (mMoveSpeedUp > 0.0f)
+        {
+            mMoveSpeedUp -= deltaTime;
+        }
+
+        if (mMoveSpeedUp < 0.0f)
+        {
+            mMoveSpeedUp = 0.0f;
+        }
+
+        if (mTurnSpeedUp > 0.0f)
+        {
+            mTurnSpeedUp -= deltaTime;
+        }
+
+        if (mTurnSpeedUp < 0.0f)
+        {
+            mTurnSpeedUp = 0.0f;
         }
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        float forwarding = Input.GetAxis("Vertical");
-        float turning = Input.GetAxis("Horizontal");
-        forwarding *= forwardSpeed * Time.fixedDeltaTime;
-        turning *= turnSpeed * Time.fixedDeltaTime;
-        if (forwarding < 0.0f) {
-            turning = -turning;
-        }
+        UpdateSpeedUp(Time.fixedDeltaTime);
 
-        // rotate model
-        transform.Rotate(Vector3.up, turning);
-
-        if (mController != null)
+        if (mStatus != null)
         {
-            // move forward
-            mController.Move(transform.forward * forwarding);
+            mStatus.Update(Time.fixedDeltaTime);
         }
     }
 }
